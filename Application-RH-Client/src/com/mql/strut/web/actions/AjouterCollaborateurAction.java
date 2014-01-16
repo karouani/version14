f868cdb1b821b0941efe1eb2cca0a3566341e8fd
KAROUANI <ykarouani@gmail.com>
2014-01-16 23:54:02 +0000

    sss ssssss
    sss   ssss

--- Application-RH-Client/src/com/mql/strut/web/actions/AjouterCollaborateurAction.java
+++ Application-RH-Client/src/com/mql/strut/web/actions/AjouterCollaborateurAction.java
@@ -16,38 +16,40 @@ import com.mql.strut.web.models.Manager;
 import com.mql.strut.web.models.User;
 import com.opensymphony.xwork2.ModelDriven;
 import com.sqli.challange.entity.Collaborateur;
+import com.sqli.challange.entity.Collaborateurs;
 import com.sqli.challange.entity.Rituls;
 import com.sqli.challange.sessions.IManagerRemote;
 
 @Stateless
 public class AjouterCollaborateurAction implements ModelDriven<CollaborateurModel>, SessionAware{
-	
+
 	private String messageCollaborateur;
 	private String menu;
-	
-	private CollaborateurModel colab;
-	
-	private Collaborateur col;
 
+	private CollaborateurModel colab;
+
+	private Collaborateur col;
+	private Long idColaborateur;
+	
 	private Long bu;
 	private Long site;
 	private List<String> techno;
 	private List<String> diplome;
 	private Long manageractuel;
-	
+
 	private String emailGmail;
 	private String pass;
 	private Long idManager;
-	
+
 	private Map<String, Object> session;
-	
+
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
 	}
-	
+
 	@EJB
 	private IManagerRemote manager;
-	
+
 	public AjouterCollaborateurAction() {
 		colab = new CollaborateurModel();
 	}
@@ -55,7 +57,7 @@ public class AjouterCollaborateurAction implements ModelDriven<CollaborateurMode
 	public void setMenu(String type) {
 		this.menu = type;
 	}
-	
+
 	public Long getIdManager() {
 		return idManager;
 	}
@@ -86,7 +88,7 @@ public class AjouterCollaborateurAction implements ModelDriven<CollaborateurMode
 	public void setDiplome(List<String> diplome) {
 		this.diplome = diplome;
 	}
-	
+
 	//Methode d'injection des listes
 	public void remplirTechno() {
 		techno = new ArrayList<>();
@@ -112,64 +114,70 @@ public class AjouterCollaborateurAction implements ModelDriven<CollaborateurMode
 	public String execute(){
 		this.remplirTechno();
 		this.remplirDiplome();
-		
+
 		Gmail gmailSession = (Gmail) session.get("gmail");
+		
 		if(gmailSession == null){
 			Gmail gm = new Gmail(emailGmail, pass);
 			session.put("gmail", gm);
 		}
+		
+		User s = (User) session.get("user");
+		
 		col = new Collaborateur(colab.getMatricule(), 
-								colab.getNom(), 
-								colab.getPrenom(), 
-								colab.getAbreviation(), 
-								colab.getSexe(), 
-								colab.getDateembauche(), 
-								colab.getMoisBAP(), 
-								colab.getParticipeseminaire(), 
-								colab.getDateparticipeseminaire(), 
-								colab.getSalaireactuel(), 
-								colab.getPosttravail(), 
-								colab.getEmail());
+				colab.getNom(), 
+				colab.getPrenom(), 
+				colab.getAbreviation(), 
+				colab.getSexe(), 
+				colab.getDateembauche(), 
+				colab.getMoisBAP(), 
+				colab.getParticipeseminaire(), 
+				colab.getDateparticipeseminaire(), 
+				colab.getSalaireactuel(), 
+				colab.getPosttravail(), 
+				colab.getEmail());
+		idColaborateur = (long) manager.ajouterCollaborateur(col, techno, diplome, bu, site, manageractuel);
+		messageCollaborateur = "Collaborateur ajoutÅ½ avec success !!";
+		menu = "nouveauCollaborateur";
+
+		String desc = "designation,notification,bienvenue";
+		String theme = "Designation comme Manager RH,Collaboratuer "+col+"success,bienvenue";
+		String risala = "Vous etes choisi comme designateur du Collaborateur "+col +
+				"Le collaborateur bien ajouter avec success" +
+				"Bienvenue dans notre Societe";
 		
+		String sujet1 = desc.split(",")[0];
+		String sujet2 = desc.split(",")[1];
+		String sujet3 = desc.split(",")[2];
+		
+		String theme1 = theme.split(",")[0];
+		String theme2 = theme.split(",")[1];
+		String theme3 = theme.split(",")[2];
+
+		String message1 = risala.split(",")[0];
+		String message2 = risala.split(",")[1];
+		String message3 = risala.split(",")[2];
+		
+		String emailMan = null ;
+		System.out.println(this.idManager);
+		
+		for (int i = 0; i < manager.consulterlistCollaborateur().size(); i++) {
+			if(s.getId() == manager.consulterlistCollaborateur().get(i).getCodecol())
+				emailMan = manager.consulterlistCollaborateur().get(i).getEmail();
+		}
+		
+		System.out.println(this.emailGmail+"----"+ this.pass+"----"+ col.getManageractuel().getEmail()+"----"+ sujet1+"----"+  message1);
+		System.out.println(this.emailGmail +"----"+ this.pass +"----"+ emailMan +"----"+ sujet2 +"----"+ message2);
+		
+		
+		SendMail.mail(this.emailGmail, this.pass,col.getManageractuel().getEmail(),sujet1, message1, null);
+		SendMail.mail(this.emailGmail, this.pass, emailMan,sujet2, message2, null);
+		SendMail.mail(this.emailGmail, this.pass, col.getEmail(),sujet3, message3, null);
 		
 	
-			long idcol;
-			//idcol= manager.ajouterCollaborateur(col, techno, diplome, bu, site, manageractuel);
-			//System.out.println(">>>>>>>>>>>>>>>>  get code apres l'ajouterCollaborateur() "+col.getCodecol());
-			
-			Long idrt = manager.organiserRitules(
-									idcol, 
-									col.getManageractuel().getCodecol(), 
-									idManager
-									);
-			//String recipiant = col.getEmail()+","+col.getManageractuel().getEmail();
-			
-			Rituls r = manager.consulterRituls(idrt);
-			
-			String sujet1 = r.getDescrt().split(",")[0];
-			String sujet2 = r.getDescrt().split(",")[1];
-			String sujet3 = r.getDescrt().split(",")[2];
-			
-			String message1 = r.getMessage().split(",")[0];
-			String message2 = r.getMessage().split(",")[1];
-			String message3 = r.getMessage().split(",")[2];
-			
-			String emailMan = null ;
-			for (int i = 0; i < manager.consulterlistCollaborateur().size(); i++) {
-				if(this.idManager.equals(manager.consulterlistCollaborateur().get(i).getCodecol()))
-					emailMan = manager.consulterlistCollaborateur().get(i).getEmail();
-			}
-			
-			SendMail.mail(this.emailGmail, this.pass,col.getManageractuel().getEmail(),sujet1, message1, null);
-			SendMail.mail(this.emailGmail, this.pass, emailMan,sujet2, message2, null);
-			SendMail.mail(this.emailGmail, this.pass, col.getEmail(),sujet3, message3, null);
-			*/
-			messageCollaborateur = "Collaborateur ajoutŽ avec success !!";
-			menu = "nouveauCollaborateur";
-			
-		    return "success";
+		return "success";
 	}
-	
+
 	public CollaborateurModel getModel() {
 		return colab;
 	}
@@ -198,5 +206,15 @@ public class AjouterCollaborateurAction implements ModelDriven<CollaborateurMode
 	public String getMenu() {
 		return menu;
 	}
+	public Long getIdColaborateur() {
+		return idColaborateur;
+	}
+	public String getEmailGmail() {
+		return emailGmail;
+	}
+	public String getPass() {
+		return pass;
+	}
 
+	
 }
